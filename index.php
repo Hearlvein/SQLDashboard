4b7fd6cdf8ae0d0d369a1797b410462b21fb1df1
Hearlvein <james.chaintron@gmail.com>
2023-12-04 18:31:10 +0100

    Update database connection credentials

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 
-require_once 'config.php';
-
 try {
-    $PDO = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+    $PDO = new PDO('mysql:host=localhost:3306;dbname=app', 'root', 'root');
 } catch (Exception $e) {
     die('Error: ' . $e->getMessage());
 }
